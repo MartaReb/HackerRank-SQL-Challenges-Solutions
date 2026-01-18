479d2de0eebe676afbed36a82a3da40899392d0a
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-01-18 16:40:03 +0100

    Create 05 - Japanese Cities' Attributes.sql

---
+++ 01 - Basic Select/05 - Japanese Cities' Attributes.sql
@@ -0,0 +1,16 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
