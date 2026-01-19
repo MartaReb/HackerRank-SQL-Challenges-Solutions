de50e0bfdc035c989e1933eefc42c5eb58e17c29
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-01-19 20:27:36 +0100

    Create 06 - Japanese Cities' Names.sql

---
+++ 01 - Basic Select/06 - Japanese Cities' Names.sql
@@ -0,0 +1,16 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
