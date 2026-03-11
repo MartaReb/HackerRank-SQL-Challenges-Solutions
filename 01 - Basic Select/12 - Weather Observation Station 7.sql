17c3bacf8bf6e42be122d2a6624b7f0d96e00511
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-03-11 18:14:04 +0100

    Create 12 - Weather Observation Station 7.sql

---
+++ 01 - Basic Select/12 - Weather Observation Station 7.sql
@@ -0,0 +1,16 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) IN ('A', 'E', 'I', 'O', 'U');
