b6229dca9edcae7da6e88174398ed5e116c19b04
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-03-17 09:30:06 +0100

    Create 17 - Weather Observation Station 12.sql

---
+++ 01 - Basic Select/17 - Weather Observation Station 12.sql
@@ -0,0 +1,17 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+-- Difficulty: Easy
+-- Max Score: 15
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U')
+AND RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
