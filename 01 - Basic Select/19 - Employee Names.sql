aa4af929166dab6683087d84785375fb3d86e46a
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-03-18 08:58:36 +0100

    Create 19 - Employee Names.sql

---
+++ 01 - Basic Select/19 - Employee Names.sql
@@ -0,0 +1,16 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/name-of-employees/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT name
+FROM Employee
+ORDER BY name;
