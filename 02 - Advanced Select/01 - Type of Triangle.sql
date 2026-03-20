28330bcca6c8d8a8ef4e1b07269ba228fb3240b9
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-03-20 10:39:30 +0100

    Create 01 - Type of Triangle.sql

---
+++ 02 - Advanced Select/01 - Type of Triangle.sql
@@ -0,0 +1,21 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- Difficulty: Easy
+-- Max Score: 20
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT 
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A = B AND B = C  THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES;
