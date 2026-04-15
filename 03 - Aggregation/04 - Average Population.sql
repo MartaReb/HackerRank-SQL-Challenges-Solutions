6bcc32bcc0c5f9092f06895641f399845ad8c212
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-04-15 13:03:44 +0200

    Create 04 - Average Population.sql

---
+++ 03 - Aggregation/04 - Average Population.sql
@@ -0,0 +1,15 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/average-population/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY;
