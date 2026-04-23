bdc296716fe7ebf32751367adb86d9d282845f23
MartaReb <49365894+MartaReb@users.noreply.github.com>
2026-04-23 16:05:40 +0200

    Create 10 - Weather Observation Station 13.sql

---
+++ 03 - Aggregation/10 - Weather Observation Station 13.sql
@@ -0,0 +1,16 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/weather-observation-station-13/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: MySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
